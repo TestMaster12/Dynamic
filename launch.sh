36ea357cbc360cf91d566dc96b7600914cf8ff55
TestMaster12 <aryapc320@yahoo.com>
2016-01-26 16:38:04 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -111,5 +111,5 @@ else
     exit 1
   fi
 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/DynamicTG.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/Dynamic.lua -l 1 -E $@
 fi
